c1b71b63ff85108efb6d9b5c2415092110e855fa
Natty <natnaelmasresha@gmail.com>
2022-01-01 10:09:27 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+function downloadCV(){
+    document.getElementById("downloadCV").setAttribute("href","resources//pdf/Natnael_Masresha_Zerihun_CV.pdf");
+}
+function onClicked(id){
+    window.scrollTo(0, document.getElementById(id).offsetTop-75);
+};
